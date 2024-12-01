59cbc3afffbc24ca3df3a242e2e01e47bfa6de4c
Mery-Melikyan <mery.melikyan1973@gmail.com>
2024-12-01 23:54:55 +0400

    user another converters, f.e.slug, re_path, custom converter

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path,include
+from django.urls import path, re_path, register_converter
 from . import views
+
+from . import converters
+
+register_converter(converters.FourDigitYearConverter,type_name="year_from_4_numbers")
+#register_converter(converters.FourDigitYearConverter, type_name:"year_from_4_numbers")
 
 urlpatterns = [
     path("", views.index),
     path("categories/", views.categories), #http://127.0.0.1:8000/categories/
-    path("categories/<int:cat_id>", views.categories_by_id) #http://127.0.0.1:8000/categories/1
+    path("categories/<int:cat_id>", views.categories_by_id), #http://127.0.0.1:8000/categories/1
+    path("categories/<slug:cat_slug>", views.archive),  # http://127.0.0.1:8000/categories/news-about-armenia_12
+    path("archive/<year_from_4_numbers:year>/", views.archive)
+    #re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive),
 ]
