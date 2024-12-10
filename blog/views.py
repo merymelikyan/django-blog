5c58a1fde471fae105e3d0c868cf1edb93d61c02
Mery-Melikyan <mery.melikyan1973@gmail.com>
2024-12-10 10:49:01 +0400

    created include & template tags - single & inclusion

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ data_from_db = [
 		"title": "CSTO should not have fought for Yerevan in Karabakh, Putin says",
 		"is_published": True,
 		"content": """
-			The Collective Security Treaty Organization (CSTO) should not have fought for Yerevan in Karabakh; in this case,
+			<span style="color:red"> The Collective Security </span> Treaty Organization (CSTO) should not have fought for Yerevan in Karabakh; in this case,
 			there was no external aggression against Armenia, Russian President Vladimir Putin said, TASS reports. Also,
 			the Russian president did not rule out that Armenia will return to full-time work within the CSTO.
 			He added that Armenia has not yet announced its withdrawal from the CSTO and supports all the
@@ -57,20 +57,32 @@ data_from_db = [
 	}
 ]
 
+categories_from_db = [
+	{"id": 1, "name": "Traveling"},
+	{"id": 2, "name": "Sport"},
+	{"id": 3, "name": "IT & Development"},
+	{"id": 4, "name": "Cars"},
+	{"id": 5, "name": "Fashion"},
+]
+
 def index(request):
   data = {
     "title": "Home Page",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
 	"posts": data_from_db
   }
-  return render(request, "blog/index.html", data)
+  return render(request, "blog/home.html", data)
 
 def about(request):
   return render(request, "blog/about.html", {"title":"About Page"})
 
 
-def post_more(request, post_id):
+def show_more(request, post_id):
   return  render(request, "blog/read-more.html", {"title":"Read More"})
+
+
+def show_category(request, cat_id):
+  return  render(request, "blog/category.html", {"title":f"Category:{cat_id}"})
 
 
 def add_post(request):
